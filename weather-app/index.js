66c3a434fbbc5b819d577534d718e70a632c0477
Ярослав Коберник-Березовский <Yaroslavisgood@gmail.com>
2020-04-14 00:17:30 +0300

    Fixed mistakes

--- weather-app/index.js
+++ weather-app/index.js
@@ -4,14 +4,24 @@ const form=document.getElementById("form");
 const tableData=document.getElementById('tbody');
 let streets,map;
 const store = new Map() ;
+
+function fetchWeather(query){
+    return fetch(`http://api.weatherstack.com/current?access_key=0d7fc2d94bc4c64b3a88c7695755934c&query=${query}`)
+    .then(response=>response.json())
+}
+
+function fetchMap(lat,lon){
+return fetch(`https://eu1.locationiq.com/v1/reverse.php?key=2abdae3e44d36b&lat=${lat}&lon=${lon}&format=json`)
+.then(response =>  response.json())
+}
 function mySubmit(e){
     e.preventDefault();
 }
 form.onsubmit=mySubmit;
 const clear=document.getElementById("clear");
 button.addEventListener("click",()=>{
-    fetch(`http://api.weatherstack.com/current?access_key=0d7fc2d94bc4c64b3a88c7695755934c&query=${input.value}`)
-    .then(response => response.json())
+    
+    fetchWeather(input.value)
     .then(info => {
         store.set(info.location.name, [info.location.name, info.location.country, info.current.temperature,(info.current.temperature*1.8)+32])
         tableData.innerHTML = ''
@@ -25,8 +35,7 @@ button.addEventListener("click",()=>{
         </tr>`
         tableData.innerHTML = tableData.innerHTML + row
     })
-     fetch(`https://eu1.locationiq.com/v1/reverse.php?key=2abdae3e44d36b&lat=${info.location.lat}&lon=${info.location.lon}&format=json`)
-     .then(response =>  response.json())
+     fetchMap(info.location.lat,info.location.lon)
       .then(info=>{
         if(map && streets){
             map.remove()}
